b0b5381bb2c5df540d23c48850acb8db572e71ff
jiyoon <jiyoon02@heliosen.co.kr>
2025-08-06 16:04:26 +0900

    페이지 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
